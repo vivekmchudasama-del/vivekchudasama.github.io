4e781419395f8ed7563ab17c9618241428890bd8
Vivek Chudasama <vivek.m.chudasama@gmail.com>
2025-12-29 20:32:54 +0530

    Delete app.js

--- app.js
+++
@@ -1,4 +0,0 @@
-// visitor count
-const KEY='visitor-count';if(!localStorage.getItem('visited')){localStorage.setItem('visited','1');localStorage.setItem(KEY,(+localStorage.getItem(KEY)||0)+1)}document.getElementById('visitorCount').innerText=localStorage.getItem(KEY)||1;
-// cursor
-const c=document.querySelector('.cursor');document.addEventListener('mousemove',e=>{c.style.left=e.clientX+'px';c.style.top=e.clientY+'px'})
